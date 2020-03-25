ab387a8c79a515499a730f4b6536804cd107779f
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-25 17:16:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,23 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
 
+
+    post '/teams' do
+      @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
+        members = params[:team][:members]
+
+      members.each do |member_params|
+        SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
+      end
+      @super_heroes = SuperHero.all
+
+      erb :team
+    end
+end	end
 
 
 
